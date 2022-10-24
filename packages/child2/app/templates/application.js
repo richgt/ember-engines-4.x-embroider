7126186d638f1d42487cc85e5d9a361b56515e3e
Richard Glazerman <rglazerman@squareup.com>
2022-10-24 12:56:33 -0400

    Add files

---
+++ packages/child2/app/templates/application.js
@@ -0,0 +1,1 @@
+export { default } from 'child2/templates/application';
---
+++ packages/child2/config/environment.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = function (environment) {
+  const ENV = {
+    modulePrefix: 'child',
+    environment: environment,
+  };
+
+  return ENV;
+};
